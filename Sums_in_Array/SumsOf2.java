df0c96675dab9dab6c568ce1cbd10af9286922ee
mjjtrebing <mjjtrebing@gmail.com>
2022-03-12 15:11:44 -0500

    Committing another simple interview program

---
+++ Sums_in_Array/SumsOf2.java
@@ -0,0 +1,32 @@
+
+
+public class SumsOf2
+{
+    public static void main(String[] args)
+    {
+        int[] array = {2, 9, 18, 5, 0, 3, 6 ,9};
+        int target = 8;
+
+        int[] result = twoSum(array, target);
+
+        System.out.print("Array position [" + result[0] +", " + result[1] + "] equals " + target);
+    }
+
+    public static int[] twoSum(int[] nums, int target)
+    {
+        int[] targetSums = new int[2];
+        int sum;
+        for(int i=0; i<nums.length; i++)
+        {
+            for(int j=i+1; j<nums.length; j++)
+            {
+                if((nums[i] + nums[j]) == target)
+                {
+                    targetSums[0] = i;
+                    targetSums[1] = j;
+                }
+            }
+        }
+        return targetSums;
+    }
+}
